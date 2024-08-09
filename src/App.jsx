d9e9738b766c922a96d31999bef27a883dbe9701
NicolasER <99098906+nicohub911@users.noreply.github.com>
2024-08-09 16:41:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { Route, Link} from 'wouter'
 import './css/App.css'
-import WelCome from './components/home.jsx'
 import Story from './components/story.jsx'
 import Game from './components/game.jsx'
 import GameMenu from './components/gamemenu.jsx';
 import ControlsInfo from './components/controls.jsx';
+import { IoPlayCircleOutline } from "react-icons/io5";
 
 export default function App() {
 
 
   return (
     <div className='conteiner'>
-      <Route path='/' component={WelCome}/>
-      <Route path='/story' component={Story}/>
-      <Route path='/game_menu' component={GameMenu}/>
-      <Route path='/game' component={Game}/>
-      <Route path='/controls' component={ControlsInfo}/>
+      <div className="home__container">
+        <Link className="home__container__Link" to="/story"><IoPlayCircleOutline className="home__container__Link__icon"/></Link>
+        <Route path='/story' component={Story}/>
+        <Route path='/game_menu' component={GameMenu}/>
+        <Route path='/game' component={Game}/>
+        <Route path='/controls' component={ControlsInfo}/>
+      </div>
     </div>
   )
 }
